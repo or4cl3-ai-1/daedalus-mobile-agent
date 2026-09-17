74ee0257ab18260b7c5631dec94aeedcdb0304d5
Dustin Groves <declanmacleod228@gmail.com>
2026-09-17 12:43:30 -0700

    v2.1: HF LLM in-app settings, Docs tab, methodology modes, voice I/O, follow-ups, export, README

--- sw.js
+++ sw.js
@@ -1,16 +1,17 @@
-const CACHE_NAME = 'daedalus-v1.0';
+const CACHE_NAME = 'daedalus-v2.1';
 const ASSETS = [
   './',
   './index.html',
   './css/styles.css',
   './js/daedalus.js',
   './manifest.json',
+  './icons/logo.png',
   './icons/icon.svg'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS).catch(() => {}))
   );
   self.skipWaiting();
 });
